8f08c0eba4611e94de9aee551d3efbae6cfd386d
Prateek Giri <prateekgiri999@gmail.com>
2023-10-30 23:05:03 +0530

    Security check

---
+++ src/main/java/com/Optimus/studentlaibraymangment/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+//package com.Optimus.studentlaibraymangment.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//public class SecurityConfig {
+//
+//    @SuppressWarnings("removal")
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .oauth2Login();
+//        return http.build();
+//    }
+//}
